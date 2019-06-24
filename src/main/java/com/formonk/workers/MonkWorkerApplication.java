1e6998ba2fab79a1ed415c580c6b51b9fe490620
tanmay_majumdar <tanmay.majumdar@nowfloats.com>
2019-06-24 15:09:38 +0530

    First commit

---
+++ src/main/java/com/formonk/workers/MonkWorkerApplication.java
@@ -0,0 +1,13 @@
+package com.formonk.workers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonkWorkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonkWorkerApplication.class, args);
+	}
+
+}
